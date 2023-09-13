5d2b28bedefebda9cffd6f3a904564505edc4f15
samcode206 <hadi.sameem.dev@gmail.com>
2023-09-12 20:32:43 -0700

    wire up client to listn for stdin and pipe the data to the socket server

--- client/client.js
+++ client/client.js
@@ -1,27 +1,39 @@
 const WebSocket = require("ws");
 
-try {
-  const ws = new WebSocket("ws://127.0.0.1:9919/", {
-    perMessageDeflate: false,
-  });
+const ws = new WebSocket("ws://127.0.0.1:9919/", {
+  perMessageDeflate: false,
+});
 
-  ws.onopen = (ev) => {
-    console.log("opened");
-  };
+ws.onopen = (ev) => {
+  console.log("opened");
+};
 
-  ws.onclose = (ev) => {
-    // console.log("close", ev);
-  };
+ws.onclose = (ev) => {
+  // console.log("close", ev);
+};
 
-  ws.onerror = (err) => {
-    console.log("err", err);
-  };
+ws.onerror = (err) => {
+  console.log("err", err);
+};
 
-  ws.onmessage = (msg) => {
-    console.info("msg");
-  };
+ws.onmessage = (msg) => {
+  console.info("msg");
+};
 
-  ws.on("unexpected-response", (req, res) => {
-    console.log(res);
-  });
-} catch (err) {}
+ws.on("unexpected-response", (req, res) => {
+  console.log(res);
+});
+
+process.stdin.on("data", (data) => {
+  if (ws.OPEN) {
+    ws.send(data, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  } else if (ws.CONNECTING) {
+    console.warn("please try later connecting...");
+  } else {
+    console.error("websocket is closed or closing!");
+  }
+});
