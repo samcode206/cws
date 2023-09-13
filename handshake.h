ea7ccb88b673512258e17ac94bbc0318635c9df1
samcode206 <hadi.sameem.dev@gmail.com>
2023-09-12 20:40:25 -0700

    handshake.h cleaning and documenation

--- handshake.h
+++ handshake.h
@@ -1,17 +1,18 @@
 #ifndef HANDSHAKE_HTTP_PARSING_H
 #define HANDSHAKE_HTTP_PARSING_H
 
+#include "base64.h"
 #include <errno.h>
+#include <openssl/sha.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include "base64.h"
-#include <openssl/sha.h>
 
-
-
-#define SWITCHING_PROTOCOLS "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: "
-#define SWITCHING_PROTOCOLS_HDRS_LEN sizeof(SWITCHING_PROTOCOLS) - 1 // -1 to ignore the nul
+#define SWITCHING_PROTOCOLS                                                    \
+  "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: "     \
+  "Upgrade\r\nSec-WebSocket-Accept: "
+#define SWITCHING_PROTOCOLS_HDRS_LEN                                           \
+  sizeof(SWITCHING_PROTOCOLS) - 1 // -1 to ignore the nul
 
 #define WS_VERSION 13
 
@@ -23,49 +24,58 @@
 #define CRLF "\r\n"
 #define CRLF2 "\r\n\r\n"
 
-static int get_header(const char *headers, const char *key, char *val, size_t n) {
+static int get_header(const char *headers, const char *key, char *val,
+                      size_t n) {
   const char *header_start = strstr(headers, key);
   if (header_start) {
-    header_start = strchr(header_start, ':');
+    header_start =
+        strchr(header_start,
+               ':'); // skip colon symbol, if not found header is malformed
     if (header_start == NULL) {
       return ERR_HDR_MALFORMED;
     }
 
-    ++header_start;
+    ++header_start; // skipping colon symbol happens here after validating it's
+                    // existence in the buffer
     // skip spaces
     while (*header_start == SPACE) {
       ++header_start;
     }
 
-    const char *header_end = strstr(header_start, CRLF);
+    const char *header_end =
+        strstr(header_start, CRLF); // move to the end of the header value
     if (header_end) {
+      // if string is larger than n, return to caller with ERR_HDR_TOO_LARGE
       if ((header_end - header_start) + 1 > n) {
         return ERR_HDR_TOO_LARGE;
       }
       memcpy(val, header_start, (header_end - header_start));
-      val[header_end - header_start + 1] = '\0';
-      return header_end - header_start + 1;
+      val[header_end - header_start + 1] =
+          '\0'; // nul terminate the header value
+      return header_end - header_start +
+             1; // we only add one here because of adding '\0'
     } else {
-      return ERR_HDR_MALFORMED;
+      return ERR_HDR_MALFORMED; // if no CRLF is found the headers is malformed
     }
   }
 
-  return ERR_HDR_NOT_FOUND;
+  return ERR_HDR_NOT_FOUND; // header isn't found
 }
 
-
-static ssize_t ws_build_upgrade_headers(const char *accept_key, size_t keylen,  char * resp_headers){
+static ssize_t ws_build_upgrade_headers(const char *accept_key, size_t keylen,
+                                        char *resp_headers) {
   memcpy(resp_headers, SWITCHING_PROTOCOLS, SWITCHING_PROTOCOLS_HDRS_LEN);
-  keylen-=1;
+  keylen -= 1;
   memcpy(resp_headers + SWITCHING_PROTOCOLS_HDRS_LEN, accept_key, keylen);
-  memcpy(resp_headers + SWITCHING_PROTOCOLS_HDRS_LEN + keylen, CRLF2, sizeof(CRLF2));
+  memcpy(resp_headers + SWITCHING_PROTOCOLS_HDRS_LEN + keylen, CRLF2,
+         sizeof(CRLF2));
   return SWITCHING_PROTOCOLS_HDRS_LEN + keylen + sizeof(CRLF2);
 }
 
-
 static const char magic_str[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
-static inline int ws_derive_accept_hdr(const char *akhdr_val, char *derived_val, size_t len) {
+static inline int ws_derive_accept_hdr(const char *akhdr_val, char *derived_val,
+                                       size_t len) {
   unsigned char buf[128] = {0};
   memcpy(buf, akhdr_val, strlen(akhdr_val));
   strcat((char *)buf, magic_str);
@@ -74,9 +84,8 @@ static inline int ws_derive_accept_hdr(const char *akhdr_val, char *derived_val,
 
   unsigned char hash[20] = {0};
   SHA1(buf, len, hash);
-  
+
   return Base64encode(derived_val, (const char *)hash, sizeof hash);
 }
-
 
 #endif /* HANDSHAKE_HTTP_PARSING_H */
