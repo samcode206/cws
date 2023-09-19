3d1855764ad2f59102ba8871ae815efd65a83592
samcode206 <hadi.sameem.dev@gmail.com>
2023-09-19 12:38:44 -0700

    relocate

---
+++ test/client/ping-pong.js
@@ -0,0 +1,47 @@
+const WebSocket = require("ws");
+
+const ws = new WebSocket("ws://127.0.0.1:9919/", {
+  perMessageDeflate: false,
+  
+});
+
+ws.onopen = (ev) => {
+  console.log("opened");
+};
+
+ws.onclose = (ev) => {
+  // console.log("close", ev);
+};
+
+ws.onerror = (err) => {
+  console.log("err", err);
+};
+
+ws.onmessage = (msg) => {
+  console.info("msg");
+};
+
+ws.on("ping", (data) => {
+  console.log("ping", data.toString());
+});
+
+ws.on("pong", (data) => {
+  console.log("pong", data.toString());
+});
+ws.on("unexpected-response", (req, res) => {
+  console.log(res);
+});
+
+process.stdin.on("data", (data) => {
+  if (ws.OPEN) {
+    ws.ping(data.toString('ascii'), (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  } else if (ws.CONNECTING) {
+    console.warn("please try later connecting...");
+  } else {
+    console.error("websocket is closed or closing!");
+  }
+});
