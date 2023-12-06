6194b707dc0208109b179ba5129a586472e4e55f
samcode206 <hadi.sameem.dev@gmail.com>
2023-12-05 21:31:55 -0800

    chore

--- test/client/fragmented_test.c
+++ test/client/fragmented_test.c
@@ -1,14 +1,48 @@
+#include "../../src/ws.h"
 #include "sock_util.h"
 #include <assert.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define PORT 9919
+#define ADDR "::1"
+
+void server_on_open(ws_conn_t *conn) {}
+
+void server_on_msg(ws_conn_t *conn, void *msg, size_t n, bool bin) {
+  // printf("msg %zu\n", n);
+  ws_conn_send(conn, msg, n, 0);
+}
+
+void server_on_disconnect(ws_conn_t *conn, int err) {
+  printf("%s\n", ws_conn_strerror(conn));
+}
+
+void *server_init(void *_) {
+  struct ws_server_params p = {
+      .addr = ADDR,
+      .port = PORT,
+      .on_ws_open = server_on_open,
+      .on_ws_msg = server_on_msg,
+      .on_ws_disconnect = server_on_disconnect,
+      .max_buffered_bytes = 2048,
+      .max_conns = 2,
+  };
+
+  int stat;
+  ws_server_t *s = ws_server_create(&p, &stat);
+
+  ws_server_start(s, 1024);
+
+  return NULL;
+}
 
 static const char msg[] =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "
     "Vestibulum elementum venenatis nibh, in accumsan justo  quis. ";
 
 static const size_t msg_len = sizeof msg - 1;
-
-
 
 void test1(int fd) {
   unsigned char *first_frame = new_frame(msg, 4, OP_TXT);
@@ -37,7 +71,7 @@ void test1(int fd) {
 void do_fragmented_msg_test1() {
   printf("do_fragmented_msg_test1.........\n");
   int fd = sock_new(1);
-  sock_connect(fd, 9919, "::1", 1);
+  sock_connect(fd, PORT, ADDR, 1);
   sock_upgrade_ws(fd);
   test1(fd);
 }
@@ -75,7 +109,7 @@ void test2(int fd) {
 void do_fragmented_msg_test2() {
   printf("do_fragmented_msg_test2.........\n");
   int fd = sock_new(1);
-  sock_connect(fd, 9919, "::1", 1);
+  sock_connect(fd, PORT, ADDR, 1);
   sock_upgrade_ws(fd);
 }
 
@@ -171,7 +205,7 @@ void do_fragmented_msg_test3() {
   // send the same message 3 times first is fragmented then twice un fragmented
   // expect the message to be echoed back 3 times
   int fd = sock_new(1);
-  sock_connect(fd, 9919, "::1", 1);
+  sock_connect(fd, PORT, ADDR, 1);
   sock_upgrade_ws(fd);
 
   test3(fd);
@@ -247,7 +281,6 @@ void test5(int fd) {
   printf("[Success] Received all messages and interleaved pongs\n");
 }
 
-
 void test6(int fd) {
 
   struct iovec vecs[] = {
@@ -281,13 +314,11 @@ void test6(int fd) {
       },
   };
 
-
-  for (size_t i = 0; i < 7; i++){
-    for (size_t j = 0; j < vecs[i].iov_len; j++){
-      assert(send(fd, vecs[i].iov_base+j, 1, 0) == 1);
+  for (size_t i = 0; i < 7; i++) {
+    for (size_t j = 0; j < vecs[i].iov_len; j++) {
+      assert(send(fd, vecs[i].iov_base + j, 1, 0) == 1);
     }
   }
-
 
   char buf[512];
 
@@ -316,14 +347,20 @@ void test6(int fd) {
   printf("[Success] Received all messages and interleaved pongs\n");
 }
 
-
 int main(void) {
   assert(!(msg_len % 4));
+  pthread_t server_w;
+
+  if (pthread_create(&server_w, NULL, server_init, NULL) == -1) {
+    perror("pthread_create");
+    exit(EXIT_FAILURE);
+  };
+
+  sleep(1);
 
   int fd = sock_new(1);
-  sock_connect(fd, 9919, "::1", 1);
+  sock_connect(fd, PORT, ADDR, 1);
   sock_upgrade_ws(fd);
-
 
   printf("test6....................\n");
   test6(fd);
