daad1aa9fb86f9654f64f543f32a7a32491d33cb
samcode206 <hadi.sameem.dev@gmail.com>
2023-12-12 15:37:37 -0800

    ipv4 and ipv6

--- test/suite/create_test.c
+++ test/suite/create_test.c
@@ -9,7 +9,6 @@ void onMsg(ws_conn_t *conn, void *msg, size_t n, bool bin) {}
 
 void onDisconnect(ws_conn_t *conn, int err) {}
 
-
 int test_ws_server_create_bad_args(const char *name) {
   struct ws_server_params p = {0};
 
@@ -25,7 +24,6 @@ int test_ws_server_create_bad_args(const char *name) {
 
   return EXIT_SUCCESS;
 }
-
 
 int test_ws_server_create_no_mandatory_cbs(const char *name) {
   struct ws_server_params p = {
@@ -81,12 +79,58 @@ int test_ws_server_create_no_mandatory_cbs(const char *name) {
   }
 }
 
+int test_ws_server_create_ipv4_ipv6(const char *name) {
+  struct ws_server_params p = {
+      .port = 9917,
+      .addr = "127.0.0.1",
+      .on_ws_msg = onMsg,
+      .on_ws_open = onOpen,
+      .on_ws_disconnect = onDisconnect,
+      .max_conns = 1,
+      .max_buffered_bytes = 1,
+  };
+
+  if (ws_server_create(&p) == NULL) {
+    fprintf(stderr,
+            "[FAIL] %s : expected server to be created with ipv4 address"
+            "\n",
+            name);
+    return EXIT_FAILURE;
+  }
+
+  p.port = 9919;
+  p.addr = "::1";
+
+  if (ws_server_create(&p) == NULL) {
+    fprintf(stderr,
+            "[FAIL] %s : expected server to be created with ipv6 address"
+            "\n",
+            name);
+    return EXIT_FAILURE;
+  }
+
+  fprintf(stdout, "[PASS] %s\n", name);
+
+  return EXIT_SUCCESS;
+}
+
+void result(int *ret, int cur) {
+  if (*ret == EXIT_SUCCESS) {
+    if (cur != EXIT_SUCCESS) {
+      *ret = cur;
+    }
+  }
+}
+
 int main(void) {
-  int ret = test_ws_server_create_bad_args("ws_server_create empty params");
+  int ret = 0;
 
-  ret = test_ws_server_create_no_mandatory_cbs(
-      "ws_server_create no mandatory callbacks");
+  result(&ret, test_ws_server_create_bad_args("ws_server_create empty params"));
+  result(&ret, test_ws_server_create_no_mandatory_cbs(
+                   "ws_server_create no mandatory callbacks"));
 
+  result(&ret, test_ws_server_create_ipv4_ipv6(
+                   "ws_server_create binds to ipv4 or ipv6"));
 
   exit(ret);
 }
