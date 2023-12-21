f6db5f9355eb970d42b0171a1171fd945ab865f2
samcode206 <hadi.sameem.dev@gmail.com>
2023-12-20 17:54:57 -0800

    chore

--- examples/echo.c
+++ examples/echo.c
@@ -34,7 +34,7 @@ int main(void) {
       .on_ws_open = onOpen,
       .on_ws_msg = onMsg,
       .on_ws_disconnect = onDisconnect,
-      .max_buffered_bytes = 1024 * 128,
+      .max_buffered_bytes = 1024 * 512,
       .max_conns = MAX_CONNS,
       .verbose = 1,
   };
