f6cb955255192fdfdf2438e6483c60b2fc38de59
samcode206 <hadi.sameem.dev@gmail.com>
2023-12-24 14:27:34 -0800

    clean up example

--- examples/echo.c
+++ examples/echo.c
@@ -4,53 +4,12 @@
 #include <stdlib.h>
 #include <sys/epoll.h>
 #include <sys/signal.h>
-#include <sys/timerfd.h>
 
 #define MAX_CONNS 1024
 
-size_t total = 0;
-
 void onOpen(ws_conn_t *conn) {}
 
-void onUpgrade(ws_conn_t *conn, struct ws_conn_handshake *hs) {
-  struct http_header headers[5] = {
-      {
-          "Upgrade",
-          "websocket",
-      },
-      {
-          "Connection",
-          "Upgrade",
-      },
-      {
-          "Server",
-          "cws",
-      },
-      {
-        "Access-Control-Allow-Origin",
-        "*",
-      },
-      {
-          "Sec-WebSocket-Accept",
-          hs->sec_websocket_accept,
-      },
-  };
-
-  struct ws_conn_handshake_response res = {
-      .body = NULL,
-      .header_count = 5,
-      .per_msg_deflate = 1,
-      .status = "101 Switching Protocols",
-      .headers = headers,
-  };
-
- ws_conn_handshake_reply(conn, &res);
-
-
-}
-
 void onMsg(ws_conn_t *conn, void *msg, size_t n, uint8_t opcode) {
-  // printf("msg %zu\n", n);
   if (ws_conn_msg_ready(conn)) {
     ws_conn_put_msg(conn, msg, n, OP_BIN, 0);
   } else {
@@ -71,7 +30,6 @@ int main(void) {
       .on_ws_disconnect = onDisconnect,
       .max_buffered_bytes = 1024 * 512,
       .max_conns = MAX_CONNS,
-      .on_ws_handshake = onUpgrade,
       .verbose = 1,
   };
 
