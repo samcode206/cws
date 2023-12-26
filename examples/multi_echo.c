5106ad4fc6817892f3ad88de26dea7c5d4a6cd50
samcode206 <hadi.sameem.dev@gmail.com>
2023-12-26 11:36:14 -0800

    buf_sz

--- examples/multi_echo.c
+++ examples/multi_echo.c
@@ -46,7 +46,7 @@ int main(void) {
         .on_ws_open = onOpen,
         .on_ws_msg = onMsg,
         .on_ws_disconnect = onDisconnect,
-        .max_buffered_bytes = 1024 * 1024,
+        .max_buffered_bytes = 1024 * 512,
         .max_conns = MAX_CONNS / NUM_SERVERS,
         .silent = 0,
     };
