b51383a38b090dd0a30504528334c4177680b64f
samcode206 <hadi.sameem.dev@gmail.com>
2024-01-12 12:14:22 -0800

    log

--- e2e.sh
+++ e2e.sh
@@ -9,12 +9,13 @@ for file in "$suite_dir"/*.c; do
     base_name=$(basename "$file" .c)
     echo "Compiling E2E..."
     echo "cc ./src/*.c "$file" -O3 -march=native -mtune=native $1 -o "$suite_dir/$base_name""
-    cc ./src/*.c "$file" $1 -O3 -march=native -mtune=native -o "$suite_dir/$base_name"
+    cc -std=c11 ./src/*.c "$file" $1 -O3 -march=native -mtune=native -o "$suite_dir/$base_name"
 
     if [ $? -eq 0 ]; then
         echo "running $suite_dir/$base_name"
         "$suite_dir/$base_name"
         rm "$suite_dir/$base_name"
+        echo "done running $suite_dir/$base_name"
     else
         echo "Compilation of $file failed."
     fi
