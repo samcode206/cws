a596ee1b826a4a6023b35056c993b53f901781f7
samcode206 <hadi.sameem.dev@gmail.com>
2024-01-13 01:44:38 -0800

    clean up

--- test/e2e/async_task.c
+++ test/e2e/async_task.c
@@ -3,6 +3,8 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <fcntl.h>
 
 #if defined(__linux__)
 #include <sys/eventfd.h>
@@ -16,6 +18,8 @@
 ws_server_t *srv;
 
 _Atomic unsigned long done = 0;
+_Atomic unsigned long writes = 0;
+_Atomic unsigned long reads = 0;
 
 void server_on_open(ws_conn_t *conn) {}
 
@@ -43,17 +47,16 @@ void *server_init(void *_) {
 
   int ret = ws_server_start(srv, 1024);
 
-  printf("ws_server_start = %d\n", ret);
+  printf("ws_server_start = %s\n", !ret ? "ok" : "failed");
 
   ret = ws_server_destroy(srv);
-  printf("ws_server_destroy = %d\n", ret);
+  printf("ws_server_destroy = %s\n", !ret ? "ok" : "failed");
 
   return NULL;
 }
 
-void server_async_task4(ws_server_t *rs, void *ctx) {
+void on_tasks_done(ws_server_t *s, void *ctx) {
   int *chanid = ctx;
-  printf("Final Task 4 running for %d\n", *chanid);
 
 #if defined(__linux__)
   uint64_t v = 1;
@@ -64,20 +67,45 @@ void server_async_task4(ws_server_t *rs, void *ctx) {
   EV_SET(&ev, *chanid, EVFILT_USER, EV_ENABLE, NOTE_TRIGGER, 0, NULL);
   kevent(*chanid, &ev, 1, NULL, 0, NULL);
 #endif
+
+  printf("id: #%d done\n", *(int *)ctx);
+}
+
+struct timespec rand_duration(long atleast, long mrand) {
+  struct timespec ts = {
+      .tv_nsec = atleast + (rand() % 10 * mrand),
+      .tv_sec = 0,
+  };
+  return ts;
 }
 
 void server_async_task3(ws_server_t *rs, void *ctx) {
-  int *chanid = ctx;
-  printf("Task 3 running for %d\n", *chanid);
-  assert(srv == rs);
+  struct timespec ts = rand_duration(25000000, 50000000);
 
-  ws_server_sched_callback(rs, server_async_task4, ctx);
+  // schedule a timeout that ends this thread's work
+  printf("id: #%d task: #%d set_timeout: %zu\n", *(int *)ctx, 3,
+         (size_t)ws_server_set_timeout(rs, &ts, ctx, on_tasks_done));
 }
 
 void server_async_task2(ws_server_t *rs, void *ctx) {
   int *chanid = ctx;
-  printf("Task 2 running for %d\n", *chanid);
+
   assert(srv == rs);
+
+  int fd = open("/dev/null", O_WRONLY);
+  if (fd) {
+    char buf[4096] = {0};
+    ssize_t n;
+    while (1) {
+      if ((n = write(fd, buf, 4096)) == -1 && errno == EINTR)
+        continue;
+      if (n == 4096)
+        writes++;
+      break;
+    }
+  } else {
+    perror("server_async_task2 open");
+  }
 
   ws_server_sched_callback(rs, server_async_task3, ctx);
 }
@@ -86,6 +114,20 @@ void server_async_task(ws_server_t *rs, void *ctx) {
   int *chanid = ctx;
   printf("Task 1 running for %d\n", *chanid);
   assert(srv == rs);
+
+  int fd = open("/dev/urandom", O_RDONLY);
+  if (fd) {
+    char buf[4096];
+    ssize_t n;
+    while (1) {
+      if ((n = read(fd, buf, 4096)) == -1 && errno == EINTR)
+        continue;
+      if (n == 4096)
+        reads++;
+      break;
+    }
+    close(fd);
+  }
 
   ws_server_sched_callback(rs, server_async_task2, ctx);
 }
@@ -109,10 +151,10 @@ void *test_init(void *_) {
   assert(read(evfd, &val, 8) == 8);
 #else
   struct kevent ev;
-  // add 
+  // add
   EV_SET(&ev, evfd, EVFILT_USER, EV_ADD, 0, 0, NULL);
   kevent(evfd, &ev, 1, NULL, 0, NULL);
-  
+
   // wait
   kevent(evfd, NULL, 0, &ev, 1, NULL);
 #endif
@@ -154,11 +196,15 @@ int main() {
 
   printf("shutting down...\n");
   int ret = ws_server_shutdown(srv);
-  printf("ws_server_shutdown = %d\n", ret);
-  printf("ws_server_shuttin_down = %d\n", ws_server_shutting_down(srv));
+  printf("ws_server_shutdown: %d\n", ret);
+  printf("ws_server_shuttin_down: %s\n",
+         ws_server_shutting_down(srv) ? "true" : "false");
   pthread_join(server_w, NULL);
 
-  printf("done %zu/%zu\n", done, (unsigned long)NUM_TEST_THREADS);
+  printf("tasks done:  %zu/%zu\n", done, (unsigned long)NUM_TEST_THREADS);
+  printf("/dev/urandom reads: %zu/%zu\n", reads, (unsigned long)NUM_TEST_THREADS);
+  printf("/dev/null writes: %zu/%zu\n", writes, (unsigned long)NUM_TEST_THREADS);
+
   if (done == NUM_TEST_THREADS) {
     exit(EXIT_SUCCESS);
   } else {
