7030a1325a74d6c5d6be91ea285322c86b991931
samcode206 <hadi.sameem.dev@gmail.com>
2024-01-14 17:42:23 -0800

    broadcast example

--- examples/broadcast.c
+++ examples/broadcast.c
@@ -17,8 +17,9 @@ static void appBroadcast(App *state, char *msg, size_t len) {
         ws_conn_flush_pending(state->conns[i]);
       }
 
-      ws_conn_put_msg(state->conns[i], msg, len, OP_BIN, 0);
-      // ws_conn_send_txt(state->conns[i], msg, len, 0);
+      // ws_conn_put_msg(state->conns[i], msg, len, OP_TXT, 0);
+      // or
+      ws_conn_send_msg(state->conns[i], msg, len, OP_TXT, 0);
     }
 
     break;
